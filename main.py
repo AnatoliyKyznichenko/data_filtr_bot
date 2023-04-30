c0cb048bdec526eac09495041ffa3248c5bad27d
Kindane <null>
2023-04-30 09:04:04 +0300

    done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import csv
-import json
+import os
+
+import openpyxl
 
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 import logging
-from dotenv import dotenv_values
 from config import config_file
 from aiogram import types
 from datetime import datetime
@@ -16,6 +17,7 @@ storage = MemoryStorage()
 bot = Bot(token=config_file['token'])
 dp = Dispatcher(bot, storage=storage)
 data_filename = "test.csv"
+outfile_filename = "all_result.csv"
 
 
 def is_allowed(msg: types.Message):
@@ -42,7 +44,7 @@ async def age(message: types.Message, state: FSMContext):
     state_data = await state.get_data() # получили дату из state (тут пока что хранится только мин возраст, макс возраст это message.text)
     data = list() # тут все данные из csv файла
     # открываем файл
-    with open(str(message.chat.id) + '.csv', newline='', encoding='utf-8') as csvfile:
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in csv_data:
             data.append(row)
@@ -65,7 +67,7 @@ async def number(message: types.Message, state: FSMContext):
     state_data = await state.get_data() # получили дату из state (тут пока что хранится только мин возраст, макс возраст это message.text)
     data = list() # тут все данные из csv файла
     # открываем файл
-    with open(str(message.chat.id) + '.csv', newline='', encoding='utf-8') as csvfile:
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in csv_data:
             data.append(row)
@@ -84,7 +86,7 @@ async def number(message: types.Message, state: FSMContext):
     state_data = await state.get_data() # получили дату из state (тут пока что хранится только мин возраст, макс возраст это message.text)
     data = list() # тут все данные из csv файла
     # открываем файл
-    with open(str(message.chat.id) + '.csv', newline='', encoding='utf-8') as csvfile:
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in csv_data:
             data.append(row)
@@ -113,7 +115,7 @@ async def number(message: types.Message, state: FSMContext):
     state_data = await state.get_data() # получили дату из state (тут пока что хранится только мин возраст, макс возраст это message.text)
     data = list() # тут все данные из csv файла
     # открываем файл
-    with open(str(message.chat.id) + '.csv', newline='', encoding='utf-8') as csvfile:
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in csv_data:
             data.append(row)
@@ -127,6 +129,15 @@ async def number(message: types.Message, state: FSMContext):
             filtered_prev = r
         else:
             filtered_prev = [i for i in filtered_prev if i not in r]
+
+    whatsapp = message.text
+    if whatsapp in ['да', 'нет']:
+        r = data_filter_user(filtered_prev, 8, 'ватсап')
+        if whatsapp == 'да':
+            filtered_prev = r
+        else:
+            filtered_prev = [i for i in filtered_prev if i not in r]
+
     availiable = set([i[4] for i in filtered_prev])
     result_value = dict()
     for value in availiable:
@@ -146,9 +157,38 @@ async def number(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state='seven')
 async def count_user(message: types.Message, state: FSMContext):
+    await message.answer("Введите название файла, куда сохранять данные")
+    await state.update_data(data={"count_users": int(message.text)})
+    await state.set_state("filename")
+
+
+@dp.message_handler(state='filename')
+async def get_filename(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    await message.answer("Текущие фильтры:\n"
+                         f"Возраст: {data['age']} - {data['number']}\n"
+                         f"Регион: {data['client']}\n"
+                         f"Операторы: {data['save_user']}\n"
+                         f"Ватсап: {data['number_operator']}\n"
+                         f"Восток: {data['messager']}\n"
+                         f"Количество клиентов: {data['count_users']}\n"
+                         "Вы уверенны, что хотите получить данные?(да/нет)"
+                         )
+    await state.update_data(data={"filename": message.text})
+    await state.set_state("confirm")
+
+
+@dp.message_handler(state='confirm')
+async def count_user(message: types.Message, state: FSMContext):
+    if message.text != "да":
+        await message.answer("отмена фильтров. Начинаем заново")
+        await message.answer('Введите минимальный возраст клиентов: ')
+        await state.finish()
+        await state.set_state("one")
+        return
     data = await state.get_data()
     rm = []
-    with open(str(message.chat.id) + '.csv', newline='', encoding='utf-8') as csvfile:
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in csv_data:
             rm.append(row)
@@ -177,14 +217,29 @@ async def count_user(message: types.Message, state: FSMContext):
         else:
             rm = [i for i in rm if i not in r]
 
-    filename = f"{message.chat.id}-result.csv"
-    with open(filename, "w", encoding="utf-8") as outfile:
-        writer = csv.writer(outfile)
-        for row in rm[:int(message.text)]:
+    rm = rm[:data['count_users']]
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    for row in rm:
+        worksheet.append(row)
+    workbook.save(filename=data['filename'])
+
+    with open(outfile_filename, "a", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        for row in rm:
             writer.writerow(row)
-    await message.answer_document(document=types.InputFile(filename))
-    # await message.answer(json.dumps(rm[0:int(message.text)],
-    # ensure_ascii=False, indent=2))  # 0:int(message.text) так как это последнее состояние
+
+    # remove `rm` data from `data_filename`
+    with open(data_filename, 'r') as inp, open(data_filename+".changed", 'w') as out:
+        writer = csv.writer(out)
+        for row in csv.reader(inp):
+            if row[0].split(";") not in rm:
+                writer.writerow(row)
+    os.remove(data_filename)
+    os.rename(data_filename+".changed", data_filename)
+
+    await message.answer_document(document=types.InputFile(data['filename']))  # 0:int(message.text) так как это последнее состояние
+    os.remove(data['filename'])
     # ensure_ascii=False для кирилицы
     await state.finish()
 
@@ -223,5 +278,54 @@ def provider_filter(data, operator):
     return res
 
 
+@dp.message_handler(commands=["info"])
+async def process_info(message: types.Message):
+    await message.answer("Подождите, идет обработка данных...")
+    data = list()
+    with open(data_filename, newline='', encoding='utf-8') as csvfile:
+        csv_data = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in csv_data:
+            data.append(row)
+
+    availiable_whatsapp = len([i for i in data if i[8] == "ватсап"])
+    not_availiable_whatsapp = len([i for i in data if i[8] != "ватсап"])
+
+    availiable_v = len([i for i in data if i[7] == "восток"])
+    not_availiable_v = len([i for i in data if i[7] != "восток"])
+
+
+    availiable_towns = set([i[5] for i in data]) # список городов
+    # убираем "" из сета
+    while "" in availiable_towns:
+        availiable_towns.remove("")
+    result_towns = dict()
+    for town in availiable_towns:
+        result_towns[town] = len([i for i in data if i[5] == town])
+    string_result_towns = "\n".join([f"{town}: {count}" for town, count in result_towns.items()])
+
+    availiable_ops = set([i[4] for i in data]) # список городов
+    # убираем "" из сета
+    while "" in availiable_ops:
+        availiable_ops.remove("")
+    result_ops = dict()
+    for op in availiable_ops:
+        result_ops[op] = len([i for i in data if i[4] == op])
+    string_result_ops = "\n".join([f"{op}: {count}" for op, count in result_ops.items()])
+
+    await message.answer("Всего данных:\n"
+                         f"<b>восток:</b> {availiable_v}\n"
+                         f"<b>не восток:</b> {not_availiable_v}\n"
+                         f"<b>ватсап:</b> {availiable_whatsapp}\n"
+                         f"<b>не ватсап:</b> {not_availiable_whatsapp}\n"
+                         "<b>По регионам</b>\n"
+                         f"{string_result_towns}"
+                         f"\n<b>По операторам</b>\n"
+                         f"{string_result_ops}",
+                         parse_mode="HTML"
+                         )
+
+
 if __name__ == '__main__':
     executor.start_polling(dp)
+
+
